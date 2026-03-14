0b94906be3c01ecbd7f7061938189c75cc76793a
Ngakosso KevKev <kevingakk@gmail.com>
2026-03-14 01:55:11 +0100

    Update BASE_URL routes api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,94 +1,95 @@
-    // api.js
+// api.js
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL =
+  window.location.hostname === "localhost" ? "http://localhost:8000" : "";
 
-    // Helper générique pour gérer les requêtes
+// Helper générique pour gérer les requêtes
 async function request(url, method = "GET", body = null) {
-    const options = {
-        method,
-        headers: {
-        "Content-Type": "application/json",
-        },
-    };
+  const options = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    if (body) {
-        options.body = JSON.stringify(body);
-    }
-    try {
-        const response = await fetch(`${BASE_URL}${url}`, options);
-        return await response.json();
-    } catch(err) {
-        console.log(err)
-    }
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+  try {
+    const response = await fetch(`${BASE_URL}${url}`, options);
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-    // =====================
-    // Routes GET
-    // =====================
+// =====================
+// Routes GET
+// =====================
 
 export async function readRoot() {
-    return await request("/", "GET");
+  return await request("/", "GET");
 }
 
 export async function getMesh() {
-    return await request("/mesh", "GET");
+  return await request("/mesh", "GET");
 }
 
 export async function getInitializationParams() {
-    return await request("/initialization_params", "GET");
+  return await request("/initialization_params", "GET");
 }
 export async function getSimulationParams() {
-    return await request("/simulation_params", "GET");
+  return await request("/simulation_params", "GET");
 }
 
 export async function getPhysicalParams() {
-    return await request("/physical_params", "GET");
+  return await request("/physical_params", "GET");
 }
 
-    // =====================
-    // Routes PATCH
-    // =====================
+// =====================
+// Routes PATCH
+// =====================
 
 export async function updateInitializationParams(initializationParams) {
-    return await request("/initialization_params", "PATCH", initializationParams);
+  return await request("/initialization_params", "PATCH", initializationParams);
 }
 
 export async function updateSimulationParams(simulationParams) {
-    return await request("/simulation_params", "PATCH", simulationParams);
+  return await request("/simulation_params", "PATCH", simulationParams);
 }
 
 export async function updatePhysicalParams(physicalParams) {
-    return await request("/physical_params", "PATCH", physicalParams);
+  return await request("/physical_params", "PATCH", physicalParams);
 }
 
 export async function setTemperature(updateSelection) {
-    return await request("/temperature", "PATCH", updateSelection);
+  return await request("/temperature", "PATCH", updateSelection);
 }
 
 export async function setVapor(updateSelection) {
-    return await request("/vapor", "PATCH", updateSelection);
+  return await request("/vapor", "PATCH", updateSelection);
 }
 
 export async function setIcePotential(updateSelection) {
-    return await request("/ice_potential", "PATCH", updateSelection);
+  return await request("/ice_potential", "PATCH", updateSelection);
 }
 
 export async function setFrozenTrue(selection) {
-    return await request("/frozen/true", "PATCH", selection);
+  return await request("/frozen/true", "PATCH", selection);
 }
 
 export async function setFrozenFalse(selection) {
-    return await request("/frozen/false", "PATCH", selection);
+  return await request("/frozen/false", "PATCH", selection);
 }
 
 export async function randomize() {
-    return await request("/randomize", "PATCH");
+  return await request("/randomize", "PATCH");
 }
 
-    // =====================
-    // Route PUT
-    // =====================
+// =====================
+// Route PUT
+// =====================
 
 export async function runSimulation() {
-    return await request("/simulation", "PUT");
+  return await request("/simulation", "PUT");
 }
